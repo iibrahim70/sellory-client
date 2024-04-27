a380ef3f5b85e75fd5cd9006347ef67260786952
iibrahim70 <iibrahiim.dev@gmail.com>
2024-04-27 23:34:19 +0600

    added on selling products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import ShopByCategory from "@/components/ShopByCategory";
 import BannerOne from "@/components/promotional/BannerOne";
 import BannerTwo from "@/components/promotional/BannerTwo";
 import DealOfTheDay from "@/components/DealOfTheDay";
+import OnSellingProducts from "@/components/OnSellingProducts";
 
 const Home = () => {
   return (
@@ -12,6 +13,7 @@ const Home = () => {
       <Promotional />
       <ShopByCategory />
       <DealOfTheDay />
+      <OnSellingProducts />
       <BannerOne />
       <BannerTwo />
     </main>
